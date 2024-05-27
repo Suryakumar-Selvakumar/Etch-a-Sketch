df8324fc87da71fd4b526e62fc0165548aeb6a02
Suryakumar Selvakumar <ssuryakumar2077@gmail.com>
2024-05-27 09:10:46 -0600

    Add features New Grid and Clear Grid

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
 const container = document.querySelector(".container");
+const btnClearGrid = document.querySelector("#clear-grid");
+let maxWidth = 500;
 
-for (let i = 1; i < 257; i++) {
-  const div = document.createElement("div");
-  div.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
-  container.appendChild(div);
+function createGrid(num) {
+  container.innerHTML = "";
+  let gridSize = num * num + num;
+  let mod = num + 1;
+  for (let i = 0; i < gridSize; i++) {
+    const div = document.createElement("div");
+    if (i % mod === 0) {
+      div.style.width = "100%";
+      div.styleheight = "0";
+      div.style.border = "0";
+    } else {
+      div.style.width = maxWidth / num + "px";
+      div.style.height = maxWidth / num + "px";
+      div.style.border = "solid 1px black";
+    }
+    container.appendChild(div);
+  }
+  divElements = container.childNodes;
+
+  divElements.forEach((element) => {
+    element.addEventListener(
+      "mouseover",
+      (e) => (e.currentTarget.style.backgroundColor = "black")
+    );
+  });
+
+  btnClearGrid.addEventListener("click", () => {
+    divElements.forEach((e) => (e.style.backgroundColor = "white"));
+  });
 }
+
+createGrid(16);
+
+const btnNewGrid = document.querySelector("#new-grid");
+btnNewGrid.addEventListener("click", () => {
+  do {
+    gridValue = parseInt(prompt("Enter desired grid size:", ""), 10);
+  } while (isNaN(gridValue) || gridValue > 100 || gridValue < 1);
+  createGrid(gridValue);
+});
