28887966c2e0704e66143ca344eae0617ff30d6e
Suryakumar Selvakumar <ssuryakumar2077@gmail.com>
2024-05-27 11:25:40 -0600

    Finish Project Base

--- script.js
+++ script.js
@@ -1,6 +1,149 @@
 const container = document.querySelector(".container");
 const btnClearGrid = document.querySelector("#clear-grid");
+const btnChangeColor = document.querySelector("#change-color");
 let maxWidth = 500;
+const colorNames = [
+  "aliceblue",
+  "antiquewhite",
+  "aqua",
+  "aquamarine",
+  "azure",
+  "beige",
+  "bisque",
+  "black",
+  "blanchedalmond",
+  "blue",
+  "blueviolet",
+  "brown",
+  "burlywood",
+  "cadetblue",
+  "chartreuse",
+  "chocolate",
+  "coral",
+  "cornflowerblue",
+  "cornsilk",
+  "crimson",
+  "cyan",
+  "darkblue",
+  "darkcyan",
+  "darkgoldenrod",
+  "darkgrey",
+  "darkgreen",
+  "darkkhaki",
+  "darkmagenta",
+  "darkolivegreen",
+  "darkorange",
+  "darkorchid",
+  "darkred",
+  "darksalmon",
+  "darkseagreen",
+  "darkslateblue",
+  "darkslategrey",
+  "darkturquoise",
+  "darkviolet",
+  "deeppink",
+  "deepskyblue",
+  "dimgray",
+  "dodgerblue",
+  "firebrick",
+  "floralwhite",
+  "forestgreen",
+  "fuchsia",
+  "gainsboro",
+  "ghostwhite",
+  "gold",
+  "goldenrod",
+  "grey",
+  "green",
+  "greenyellow",
+  "honeydew",
+  "hotpink",
+  "indianred",
+  "indigo",
+  "ivory",
+  "khaki",
+  "lavender",
+  "lavenderblush",
+  "lawngreen",
+  "lemonchiffon",
+  "lightblue",
+  "lightcoral",
+  "lightcyan",
+  "lightgoldenrodyellow",
+  "lightgrey",
+  "lightgreen",
+  "lightpink",
+  "lightsalmon",
+  "lightseagreen",
+  "lightskyblue",
+  "lightslategrey",
+  "lightsteelblue",
+  "lightyellow",
+  "lime",
+  "limegreen",
+  "linen",
+  "magenta",
+  "maroon",
+  "mediumaquamarine",
+  "mediumblue",
+  "mediumorchid",
+  "mediumpurple",
+  "mediumseagreen",
+  "mediumslateblue",
+  "mediumspringgreen",
+  "mediumturquoise",
+  "mediumvioletred",
+  "midnightblue",
+  "mintcream",
+  "mistyrose",
+  "moccasin",
+  "navajowhite",
+  "navy",
+  "oldlace",
+  "olive",
+  "olivedrab",
+  "orange",
+  "orangered",
+  "orchid",
+  "palegoldenrod",
+  "palegreen",
+  "paleturquoise",
+  "palevioletred",
+  "papayawhip",
+  "peachpuff",
+  "peru",
+  "pink",
+  "plum",
+  "powderblue",
+  "purple",
+  "red",
+  "rosybrown",
+  "royalblue",
+  "saddlebrown",
+  "salmon",
+  "sandybrown",
+  "seagreen",
+  "seashell",
+  "sienna",
+  "silver",
+  "skyblue",
+  "slateblue",
+  "slategrey",
+  "snow",
+  "springgreen",
+  "steelblue",
+  "tan",
+  "teal",
+  "thistle",
+  "tomato",
+  "turquoise",
+  "violet",
+  "wheat",
+  "white",
+  "whitesmoke",
+  "yellow",
+  "yellowgreen",
+];
 
 function createGrid(num) {
   container.innerHTML = "";
@@ -22,14 +165,25 @@ function createGrid(num) {
   divElements = container.childNodes;
 
   divElements.forEach((element) => {
-    element.addEventListener(
-      "mouseover",
-      (e) => (e.currentTarget.style.backgroundColor = "black")
-    );
+    element.addEventListener("mouseover", (e) => {
+      let x = randomColor();
+      let y = randomColor();
+      let z = randomColor();
+      e.currentTarget.style.backgroundColor = `rgb(${x},${y},${z})`;
+    });
   });
 
   btnClearGrid.addEventListener("click", () => {
     divElements.forEach((e) => (e.style.backgroundColor = "white"));
+  });
+
+  btnChangeColor.addEventListener("click", () => {
+    let divColor = selectColor();
+    divElements.forEach((element) => {
+      element.addEventListener("mouseover", (e) => {
+        e.currentTarget.style.backgroundColor = `${divColor}`;
+      });
+    });
   });
 }
 
@@ -39,6 +193,20 @@ const btnNewGrid = document.querySelector("#new-grid");
 btnNewGrid.addEventListener("click", () => {
   do {
     gridValue = parseInt(prompt("Enter desired grid size:", ""), 10);
-  } while (isNaN(gridValue) || gridValue > 100 || gridValue < 1);
+  } while ((gridValue > 100 || gridValue < 1) && !isNaN(gridValue));
   createGrid(gridValue);
 });
+
+function randomColor() {
+  let rgbVal = Math.floor(Math.random() * 256);
+  return rgbVal;
+}
+
+function selectColor() {
+  let color;
+  do {
+    color = prompt("Enter desired Color:", "");
+    console.log(color);
+  } while (!colorNames.includes(color) && color !== null);
+  return color;
+}
